2d803cf4d70c69e9306704d7d86f5d14462c0af8
rizqullahnusa22 <rizqullah.nusa22@gmail.com>
2026-01-15 17:47:03 +0700

    GOOGLE CONSOLE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth overflow-x-hidden">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="tm8vdlC0XAP8dlkjFp7ENswMp_pXU_eqB45O50ak5G0">
         <title>{{ config('app.name', 'Portofolio Rizqullah') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
